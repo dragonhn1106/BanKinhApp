ca6b15dcd6678427c3245f9dbd2ecc37c9a3b323
dragonhn1106 <dragonhn1106@gmail.com>
2018-05-02 13:39:14 +0700

    update database

--- database/chuvanduong_eyewearstore_th17.sql
+++ database/chuvanduong_eyewearstore_th17.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2018 at 04:56 PM
+-- Generation Time: May 02, 2018 at 08:38 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
 
@@ -72,7 +72,9 @@ CREATE TABLE `answers` (
 
 INSERT INTO `answers` (`id`, `question_id`, `status`, `username`, `email`, `level_user`, `create_time`, `like_answer`, `content`) VALUES
 (1, 25, 1, 'admin', 'dragonhn1xxx@gmail.com', 1, '2018-04-25 16:22:30', 0, 'sao thế bạn'),
-(2, 25, 1, 'admin', 'dragonhn1xxx@gmail.com', 0, '2018-04-25 16:24:06', 0, 'xxxxx');
+(2, 25, 1, 'admin', 'dragonhn1xxx@gmail.com', 0, '2018-04-25 16:24:06', 1, 'xxxxx'),
+(3, 25, 1, 'admin', 'dragonhn1xxx@gmail.com', 0, '2018-04-25 17:29:46', 1, '321321'),
+(4, 32, 1, 'admin', 'dragonhn1xxx@gmail.com', 0, '2018-04-26 03:41:23', 1, 'chuẩn');
 
 -- --------------------------------------------------------
 
@@ -100,7 +102,25 @@ INSERT INTO `chitiethoadon` (`id_hoadon`, `id_dh`, `status`, `create_time`, `upd
 (5, 8, 1, '2018-04-21 08:18:16', '0000-00-00 00:00:00'),
 (6, 7, 1, '2018-04-21 08:18:20', '0000-00-00 00:00:00'),
 (7, 7, 1, '2018-04-21 08:18:22', '0000-00-00 00:00:00'),
-(8, 7, 1, '2018-04-21 08:18:22', '0000-00-00 00:00:00');
+(8, 7, 1, '2018-04-21 08:18:22', '0000-00-00 00:00:00'),
+(9, 9, 1, '2018-04-26 03:48:28', '0000-00-00 00:00:00'),
+(10, 9, 1, '2018-04-26 03:48:29', '0000-00-00 00:00:00'),
+(11, 9, 1, '2018-04-26 03:48:30', '0000-00-00 00:00:00'),
+(12, 9, 1, '2018-04-26 03:48:30', '0000-00-00 00:00:00'),
+(13, 9, 1, '2018-04-26 03:48:30', '0000-00-00 00:00:00'),
+(14, 2, 1, '2018-04-26 03:51:13', '0000-00-00 00:00:00'),
+(15, 4, 1, '2018-04-26 03:51:18', '0000-00-00 00:00:00'),
+(16, 4, 1, '2018-04-26 03:51:20', '0000-00-00 00:00:00'),
+(17, 4, 1, '2018-04-26 03:51:22', '0000-00-00 00:00:00'),
+(18, 4, 1, '2018-04-26 03:51:22', '0000-00-00 00:00:00'),
+(19, 4, 1, '2018-04-26 03:51:22', '0000-00-00 00:00:00'),
+(20, 4, 1, '2018-04-26 03:51:23', '0000-00-00 00:00:00'),
+(21, 4, 1, '2018-04-26 03:51:23', '0000-00-00 00:00:00'),
+(22, 3, 1, '2018-04-26 03:53:34', '0000-00-00 00:00:00'),
+(23, 9, 1, '2018-04-26 03:53:39', '0000-00-00 00:00:00'),
+(24, 8, 1, '2018-04-26 03:53:43', '0000-00-00 00:00:00'),
+(25, 10, 1, '2018-04-26 03:53:46', '0000-00-00 00:00:00'),
+(26, 11, 1, '2018-04-26 03:53:49', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -128,17 +148,20 @@ CREATE TABLE `donhang` (
 --
 
 INSERT INTO `donhang` (`id_hd`, `id_kinh`, `TenKH`, `SDT`, `Email`, `DiaChi`, `GhiChu`, `SoLuong`, `ThanhTien`, `TrangThai`, `create_time`, `update_time`) VALUES
-(1, 1, 'duongcv', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang nhanh', 100, 6900000, 1, '2018-03-26 13:10:26', '2018-03-26 13:10:29'),
+(1, 1, 'duongcv', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang nhanh', 100, 6900000, 0, '2018-03-26 13:10:26', '2018-03-26 13:10:29'),
 (2, 2, 'duongcv', 964014502, 'duongcv@gmail.com', 'hanoi', 'dathang', 100, 69000000, 1, '2018-03-26 13:10:20', '2018-03-26 13:10:23'),
-(3, 3, 'duongcv2', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang nhanh', 1000, 6900000, 1, '2018-03-26 13:10:13', '2018-03-26 13:10:17'),
+(3, 3, 'duongcv2', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang nhanh', 1000, 6900000, 2, '2018-03-26 13:10:13', '2018-03-26 13:10:17'),
 (4, 2, 'duongcv4', 964014502, 'duongcv@gmail.com', 'hanoi', 'dathang', 2, 12000000, 1, '2018-03-26 13:10:06', '2018-03-26 13:10:10'),
-(5, 21, 'duongcv3', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang gap', 4, 120000000, 1, '2018-03-26 13:10:01', '2018-03-26 13:10:03'),
+(5, 21, 'duongcv3', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang gap', 4, 120000000, 0, '2018-03-26 13:10:01', '2018-03-26 13:10:03'),
 (6, 21, 'duongcv5', 964014502, 'duongcv@gmail.com', 'hanoi', 'dat hang gap', 4, 120000000, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
 (7, 8, 'duongcv', 964014502, 'duongcv1006@gmail.com', 'hanoij', 'dat hanh nhanh', 2, 2000000, 2, '2018-04-20 03:32:25', '0000-00-00 00:00:00'),
-(8, 11, 'duongcv', 964014502, 'duongcv1006@gmail.com', 'hanoij', 'dat hanh nhanh', 1, 1000000, 2, '2018-04-20 03:32:25', '0000-00-00 00:00:00'),
-(9, 8, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', '', 1, 1000002, 0, '2018-04-25 15:08:15', '0000-00-00 00:00:00'),
-(10, 11, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', 'sadsadsa', 1, 1000002, 0, '2018-04-25 15:09:06', '0000-00-00 00:00:00'),
-(11, 11, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', 'nhanh e ơi', 100, 100000200, 0, '2018-04-25 15:37:13', '0000-00-00 00:00:00');
+(8, 11, 'duongcv', 964014502, 'duongcv1006@gmail.com', 'hanoij', 'dat hanh nhanh', 1, 1000000, 1, '2018-04-20 03:32:25', '0000-00-00 00:00:00'),
+(9, 8, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', '', 1, 1000002, 1, '2018-04-25 15:08:15', '0000-00-00 00:00:00'),
+(10, 11, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', 'sadsadsa', 1, 1000002, 1, '2018-04-25 15:09:06', '0000-00-00 00:00:00'),
+(11, 11, 'duongcv5', 964014505, 'dragonhn1xxx@gmail.com', 'hanoi', 'nhanh e ơi', 100, 100000200, 1, '2018-04-25 15:37:13', '0000-00-00 00:00:00'),
+(12, 10, 'duongcv5', 964014505, 'dragonhn1106@gmail.com', 'hanoi', '', 1, 1000000, 0, '2018-04-26 04:49:32', '0000-00-00 00:00:00'),
+(13, 8, 'duongcv5', 964014505, 'dragonhn1106@gmail.com', 'hanoi', '', 1, 1000002, 0, '2018-04-26 04:49:33', '0000-00-00 00:00:00'),
+(14, 11, 'duongcv5', 964014505, 'dragonhn1106@gmail.com', 'hanoi', '', 1, 1000002, 0, '2018-04-26 04:49:33', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -167,17 +190,30 @@ CREATE TABLE `kinh` (
 --
 
 INSERT INTO `kinh` (`id`, `TenKinh`, `id_nsx`, `id_npp`, `HinhAnh`, `GiaCu`, `GiaMoi`, `id_loai`, `status`, `SoLuong`, `SoLuotXem`, `create_time`, `date_time`) VALUES
-(0, '', '', 0, NULL, 11, NULL, 0, 0, 0, 0, NULL, NULL),
-(2, 'T8200696 - K&Iacute;NH THỜI TRANG NAM NỮ1', '1', 1, 'kieu_kinh_1.1.jpg', 1000, 1000, 1, 1, 1, 8, '0000-00-00 00:00:00', '2018-04-10 16:59:57'),
-(3, 'T8200696 - K&Iacute;NH THỜI TRANG NAM NỮ2', '2', 2, 'kieu_kinh_1.1.jpg', 1000, 1000, 1, 1, 1, 0, '0000-00-00 00:00:00', '2018-04-20 03:30:36'),
-(4, 'T8200696 - K&Iacute;NH THỜI TRANG NAM NỮ3', '3', 3, 'cung_loai_1.jpg', 1000, 3000000, 1, 1, 1, 0, '0000-00-00 00:00:00', '2018-04-24 09:39:48'),
-(5, 'T8200696 - K&Iacute;NH THỜI TRANG NAM NỮ4', '4', 4, 'kieu_kinh_2.1.jpg', 1000, 900000, 1, 1, 1, 6, '0000-00-00 00:00:00', '2018-04-24 09:40:02'),
-(6, 'PORSCHE- 18K- P8499 - K&Iacute;NH THỜI TRANG NAM', '5', 5, 'kieu_kinh_1.2.jpg', 1000000, 900000, 3, 1, 321, 2, '2018-04-10 16:15:51', '2018-04-11 20:54:05'),
-(7, 'PORSCHE- 18K- P8499 - K&Iacute;NH THỜI TRANG NAM1', '1', 1, 'kieu_kinh_1.jpg', 1000000, 2147483647, 3, 1, 321, 3, '2018-04-10 16:16:39', '2018-04-11 20:53:14'),
-(8, 'PORSCHE-18K-P8181 - GỌNG K&Iacute;NH NỮ5', '2', 2, 'kieu_kinh_1.jpg', 1000002, 900000, 2, 1, 321, 1, '2018-04-10 16:17:25', '2018-04-24 09:40:19'),
-(9, 'PORSCHE-18K-P8181 - GỌNG K&Iacute;NH NỮ6', '3', 3, 'kieu_kinh_1.jpg', 1000000, 900000, 2, 1, 321, 33, '2018-04-10 16:17:25', '2018-04-10 16:59:43'),
-(10, 'PORSCHE-18K-P8181 - GỌNG K&Iacute;NH NỮ7', '4', 4, 'kieu_kinh_1.jpg', 1000000, 900000, 2, 1, 321, 75, '2018-04-10 16:17:25', '2018-04-10 16:59:43'),
-(11, 'PORSCHE-18K-P8181 - GỌNG K&Iacute;NH NỮ8', '5', 5, 'kieu_kinh_1.jpg', 1000002, 1000002000, 2, 1, 321, 44, '2018-04-10 16:17:25', '2018-04-10 16:59:43');
+(1, 'GG0078SK-001', '1', 1, 'GG0078SK-001-9tr.jpg', 9000000, 0, 1, 1, 1000, 0, '2018-04-27 15:23:36', '0000-00-00 00:00:00'),
+(2, 'GG3595FS-807JJ', '1', 1, 'GG3595FS-807JJ-6tr8.jpg', 6800000, 0, 2, 1, 1000, 0, '2018-04-27 15:26:00', '0000-00-00 00:00:00'),
+(3, 'GG3658FS-D28ED', '1', 1, 'GG3658FS-D28ED-7tr2.jpg', 7200000, 0, 2, 1, 100, 0, '2018-04-27 15:26:40', '0000-00-00 00:00:00'),
+(4, 'GG3661FS-75QED', '1', 1, 'GG3661FS-75QED-8tr2.JPG', 8200000, 0, 2, 1, 20, 0, '2018-04-27 15:32:01', '0000-00-00 00:00:00'),
+(5, 'GG3662KS-75QHD-B', '1', 1, 'GG3662KS-75QHD-B-8tr2.jpg', 8200000, 0, 2, 1, 100, 0, '2018-04-27 15:32:32', '0000-00-00 00:00:00'),
+(6, 'GG3665FS-51NYR', '1', 1, 'GG3665FS-51NYR-6tr2.jpg', 6200000, 0, 1, 1, 100, 0, '2018-04-27 15:33:19', '0000-00-00 00:00:00'),
+(7, 'GG3687FS-4WJYY', '1', 1, 'GG3687FS-4WJYY-6tr9.jpg', 6900000, 0, 2, 1, 100, 4, '2018-04-27 15:33:49', '0000-00-00 00:00:00'),
+(8, 'GG3776FS-MJ9-EU-6tr2', '1', 1, 'GG3776FS-MJ9-EU-6tr2.jpg', 6200000, 0, 0, 1, 20, 0, '2018-04-27 15:34:21', '0000-00-00 00:00:00'),
+(9, 'GG3796FS-ANWY1', '2', 1, 'GG3796FS-ANWY1-7tr9 - Copy.jpg', 7900000, 0, 2, 1, 100, 0, '2018-04-27 15:35:10', '0000-00-00 00:00:00'),
+(21, 'COQUETE2-XCTD8', '3', 3, 'COQUETE2-XCTD8-7tr2.jpg', 7200000, 0, 1, 1, 100, 0, '2018-04-27 15:39:39', '0000-00-00 00:00:00'),
+(22, 'DIORSYMBOL1-D289C', '3', 3, 'DIORSYMBOL1-D289C-7tr350.jpg', 7350000, 0, 2, 1, 20, 0, '2018-04-27 15:40:40', '0000-00-00 00:00:00'),
+(23, 'DIORSYMBOL2-D28JJ', '4', 3, 'DIORSYMBOL2-D28JJ-7tr350.jpg', 7350000, 0, 2, 1, 100, 0, '2018-04-27 15:41:10', '0000-00-00 00:00:00'),
+(24, 'DIORTIEDYE1-BPEHA', '5', 3, 'DIORTIEDYE1-BPEHA-7tr.jpg', 7000000, 0, 2, 1, 20, 0, '2018-04-27 15:41:40', '0000-00-00 00:00:00'),
+(25, 'DIORVOLUTE2F-BLQFM', '4', 3, 'DIORVOLUTE2F-BLQFM-7tr6.jpg', 7600000, 0, 0, 1, 10, 0, '2018-04-27 15:42:18', '0000-00-00 00:00:00'),
+(26, 'DIORZENAIDE', '2', 3, 'DIORZENAIDE-7tr350.jpg', 7350000, 0, 1, 1, 100, 0, '2018-04-27 15:43:07', '0000-00-00 00:00:00'),
+(27, 'MYLADYDIOR3SF-FCUJD', '2', 3, 'MYLADYDIOR3SF-FCUJD-7tr9.jpg', 7900000, 0, 2, 1, 10, 0, '2018-04-27 15:43:57', '0000-00-00 00:00:00'),
+(28, 'SYMBLYDIORF-D28', '2', 3, 'SYMBLYDIORF-D28-tr.jpg', 7900000, 0, 2, 1, 10, 0, '2018-04-27 15:44:27', '0000-00-00 00:00:00'),
+(29, 'SYMPLYDIOR-9E1HA', '3', 1, 'SYMPLYDIOR-9E1HA-7tr.jpg', 7000000, 0, 2, 1, 30, 0, '2018-04-27 15:44:56', '0000-00-00 00:00:00'),
+(30, 'GU400-D-01A', '4', 2, 'GU400-D-01A-2tr6.jpg', 2600000, 0, 2, 1, 200, 0, '2018-04-27 15:45:46', '0000-00-00 00:00:00'),
+(31, 'GU4002-D-05X', '2', 2, 'GU4002-D-05X-2tr6.jpg', 2600000, 0, 1, 1, 100, 0, '2018-04-27 15:46:12', '0000-00-00 00:00:00'),
+(32, 'GU6834-32N-2tr6', '5', 2, 'GU6834-32N-2tr6.jpg', 2600000, 0, 2, 1, 10, 0, '2018-04-27 15:46:42', '0000-00-00 00:00:00'),
+(33, 'GU6908-01A', '5', 2, 'GU6908-01A-2tr6.jpg', 2600000, 0, 1, 1, 10, 0, '2018-04-27 15:47:27', '0000-00-00 00:00:00'),
+(34, 'GU7433-02C', '5', 2, 'GU7433-02C-1tr9.jpg', 100, 0, 2, 1, 0, 0, '2018-04-27 15:47:54', '0000-00-00 00:00:00'),
+(35, 'GU7433-52C', '2', 2, 'GU7433-52C-1tr9.jpg', 1900000, 0, 2, 1, 10, 5, '2018-04-27 15:48:33', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -199,12 +235,11 @@ CREATE TABLE `loaikinh` (
 --
 
 INSERT INTO `loaikinh` (`id_loai`, `TenLoai`, `img_path`, `status`, `create_time`, `update_time`) VALUES
-(1, 'K&iacute;nh mắt thời trang nam', 'kieu_kinh_1.2.jpg', 0, '0000-00-00 00:00:00', '2018-04-04 08:58:54'),
-(2, 'K&iacute;nh mắt thời trang nữ 2', 'kieu_kinh_1.2.jpg', 1, '0000-00-00 00:00:00', '2018-04-08 20:16:30'),
+(1, 'Kính thời trang nam', 'kieu_kinh_1.2.jpg', 1, '0000-00-00 00:00:00', '2018-04-04 08:58:54'),
+(2, 'Kính thời trang nữ', 'kieu_kinh_1.2.jpg', 1, '0000-00-00 00:00:00', '2018-04-08 20:16:30'),
 (3, 'Kính mắt dành cho trẻ em', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:26:55', '2018-03-26 00:26:58'),
 (4, 'Kính thuốc', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:27:34', '2018-03-26 00:27:37'),
-(5, 'Kính áp tròng', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:27:57', '2018-03-26 00:28:00'),
-(6, 'k&iacute;nh mắt thời trang kute', 'kieu_kinh_1.2.jpg', 0, '2018-04-04 08:52:05', '0000-00-00 00:00:00');
+(5, 'Kính áp tròng', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:27:57', '2018-03-26 00:28:00');
 
 -- --------------------------------------------------------
 
@@ -258,15 +293,12 @@ CREATE TABLE `nhasanxuat` (
 --
 
 INSERT INTO `nhasanxuat` (`id_nsx`, `TenNSX`, `SDTNSX`, `DiaChiNSX`, `img_path`, `status`, `create_time`, `update_time`) VALUES
-(1, 'Gucci', '0964014502', 'USA_American', 'kieu_kinh_1.1.jpg', 0, '0000-00-00 00:00:00', '2018-04-10 09:09:01'),
-(2, 'Coach', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 0, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
+(1, 'Gucci', '0964014502', 'USA_American', 'kieu_kinh_1.1.jpg', 1, '0000-00-00 00:00:00', '2018-04-10 09:09:01'),
+(2, 'Guess', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
 (3, 'Christian Dior', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:30:42', '2018-03-26 00:30:44'),
-(4, 'Dolce & Gabbana', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
-(5, 'RayBan', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
-(6, 'guccci-gang', '0964014504', '321321312', 'kieu_kinh_1.2.jpg', 1, '2018-04-04 10:27:43', '0000-00-00 00:00:00'),
-(7, '1321321321', '0964014501', '321321321321312', 'kieu_kinh_1.2.jpg', 0, '2018-04-04 10:28:24', '0000-00-00 00:00:00'),
-(8, 'duongcv', '0964014506', 'h&agrave; n?i', 'kieu_kinh_1.2.jpg', 1, '2018-04-04 10:31:28', '0000-00-00 00:00:00'),
-(9, 'k&iacute;nh mắt h&agrave; nội', '0964014507', 'h&agrave; n?i', 'kieu_kinh_1.2.jpg', 1, '2018-04-04 10:32:21', '0000-00-00 00:00:00');
+(4, 'Lacoste', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
+(5, 'Levis', '0964014502', 'USA', 'kieu_kinh_1.2.jpg', 1, '2018-03-26 00:29:55', '2018-03-26 00:29:57'),
+(6, 'Rayban', '0964014504', '321321312', 'kieu_kinh_1.2.jpg', 1, '2018-04-04 10:27:43', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -281,7 +313,7 @@ CREATE TABLE `questions` (
   `content` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `create_time` datetime NOT NULL,
   `id_kinh` int(10) UNSIGNED NOT NULL,
-  `status` tinyint(4) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT '1',
   `like_comment` int(10) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -290,9 +322,10 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`id`, `username`, `email`, `content`, `create_time`, `id_kinh`, `status`, `like_comment`) VALUES
-(24, 'admin', 'dragonhn1xxx@gmail.com', '321321321', '2018-04-25 15:55:02', 28, 1, 0),
-(25, 'admin', 'dragonhn1xxx@gmail.com', 'duongcv', '2018-04-25 16:09:52', 10, 1, 0),
-(26, '', '', '', '0000-00-00 00:00:00', 0, 0, NULL);
+(1, 'admin', 'dragonhn1xxx@gmail.com', 'kính này đẹp không bạn ơi', '2018-04-25 15:55:02', 28, 1, 1),
+(2, 'admin', 'dragonhn1xxx@gmail.com', 'kính này đep đẹp trai đấy', '2018-04-25 16:09:52', 10, 1, 12),
+(3, 'admin', 'dragonhn1xxx@gmail.com', 'kính này còn bao nhiêu cái addmin ơi', '2018-04-25 17:48:13', 10, 1, 1111),
+(32, 'admin', 'dragonhn1xxx@gmail.com', 'nhung xinh đẹp', '2018-04-26 03:40:41', 10, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -408,27 +441,27 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `answers`
 --
 ALTER TABLE `answers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `chitiethoadon`
 --
 ALTER TABLE `chitiethoadon`
-  MODIFY `id_hoadon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_hoadon` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 --
 -- AUTO_INCREMENT for table `donhang`
 --
 ALTER TABLE `donhang`
-  MODIFY `id_hd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_hd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `kinh`
 --
 ALTER TABLE `kinh`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 --
 -- AUTO_INCREMENT for table `loaikinh`
 --
 ALTER TABLE `loaikinh`
-  MODIFY `id_loai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_loai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `nhaphanphoi`
 --
@@ -438,12 +471,12 @@ ALTER TABLE `nhaphanphoi`
 -- AUTO_INCREMENT for table `nhasanxuat`
 --
 ALTER TABLE `nhasanxuat`
-  MODIFY `id_nsx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_nsx` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `questions`
 --
 ALTER TABLE `questions`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 --
 -- AUTO_INCREMENT for table `taikhoan`
 --
